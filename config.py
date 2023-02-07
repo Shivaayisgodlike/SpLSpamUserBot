15ecd3d1b8d5c2b3a19f6e9d7c9a87bdcbb8e5c9
Shivaayisgodlike <124656905+Shivaayisgodlike@users.noreply.github.com>
2023-02-07 11:59:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class API:
-    API_ID = int(os.getenv("API_ID", "24427150"))
-    API_HASH = os.getenv("API_HASH", "9fcc60263a946ef550d11406667404fa")
+    API_ID = int(os.getenv("API_ID", "27793831"))
+    API_HASH = os.getenv("API_HASH", "4a101e0880f6a511f5b19e05edec3cc5")
 
 class TOKENS:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5955719787:AAHM_L9xA1cxRuceFWuKaK2vqmvAmXyhT6Q")
-    STRING_SESSION = os.getenv("STRING_SESSION", "BQCukGbS20kJrKMNneKwNR7YnrRmoHsNOYqVE7mGvW4lIDH1OkZBJvmNLHWUANoB0_RyLVI9TrsKGGjptiEDOmxdsxtfH2GG628hFnH92rcLurSdD3AJTrVj_yz0BxdiwTJFZWGqGR7ntTjAoL6JH_FFDBwBbPT5EIlxQZzBvhDt47firak0UF4GVpN98i039DETnZCbpGESN2T1ljETS92UGkxRZhz2LVkt6YSIqLsdwUkIPZSFl52lW-lhDpwJio3wtkxM_0-CwmT2yYscEVoYyuMNe6iIxLkajO2u3YPNMEia6TodhjovIeEvjCScPo7RllQP0C8iXaRwAGotAz7dAAAAATRCdh4A")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "6088731540:AAGvrZdi78-lNRq7x5hy3KAJ88xx7G81ya4")
+    STRING_SESSION = os.getenv("STRING_SESSION", "BQCeGMmfffnoAgJ6vR9LNDSPbrY30rHrJgsi_778Jk5wUOOJKNwemJLOiJWrkq_G99v6kUioxOB5eidb9vyc6Km-pfU0MQhwF3XyY1rfU_01WhA-mupOoUZYaqdB0QtEz0cMiT9pLrOdN7xp5pWNBWYUI5Nqk3pnkZSSG7YEPujoQr8JQIJqEPlbR-2bk5u4sAGKN7JoLlyZ_GHyhmtbWKup4w49MZ3WuLzMcUMyc7L4guNj3JTh2iWyndIvnaZKLbsW_m_WvVaG2tGWI0cB4NLwPShWUHaajAXLlir7z3_Mrek0NqUjZ1BmabL4ZPERVX9zIzm-cfsZN0g4LSliFZG5AAAAAVEVII0A")
     STRING_SESSION_2 = os.getenv("STRING_SESSION_2", "")
     STRING_SESSION_3 = os.getenv("STRING_SESSION_3", "")
     STRING_SESSION_4 = os.getenv("STRING_SESSION_4", "")
@@ -18,13 +18,13 @@ class TOKENS:
     STRING_SESSION_10 = os.getenv("STRING_SESSION_10", "")
 
 class DATABASE:
-    MONGO_DB_URL = os.getenv("MONGO_DB_URL", "mongodb+srv://alpha:<password>@cluster0.oglyxsf.mongodb.net/?retryWrites=true&w=majority")
+    MONGO_DB_URL = os.getenv("MONGO_DB_URL", "mongodb+srv://Hi_shivaay:<password>@cluster0.mdctz5d.mongodb.net/?retryWrites=true&w=majority")
 
 class DEV:
-    OWNER_ID = int(os.getenv("OWNER_ID", "5629151684"))
+    OWNER_ID = int(os.getenv("OWNER_ID", "5655306381"))
     
     # DONT EDIT THIS 
-    SUDO_USERS = [5868832590] 
+    SUDO_USERS = [5767907357] 
     # YOU CAN ADD SUDO USING /addsudo
 
 class STUFF:
